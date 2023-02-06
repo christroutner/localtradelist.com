9bb26dc845f949924b047460bfc27f347a566a14
Chris Troutner <chris.troutner@gmail.com>
2023-02-06 15:44:08 -0800

    Added waiting modal to delete product button

--- src/components/edit-mutable-data/products/delete-product.js
+++ src/components/edit-mutable-data/products/delete-product.js
@@ -12,9 +12,17 @@ import { Button} from 'react-bootstrap'
 import updateMutableData from '../update-mutable-data.js'
 import SspApi from '../../../services/ssp-api'
 import ModalContinueCancel from '../../confirm-modal'
+import ModalWaiting from '../../waiting-modal'
 
 function DeleteProduct(props) {
+  // State for Confirm/Cancel Modal
   const [showCCModal, setShowCCModal] = useState(false)
+
+  // State of waiting modal
+  const [showWaitingModal, setShowWaitingModal] = useState(false)
+  const [waitingModalBody, setWaitingModalBody] = useState([])
+  const [hideSpinner, setHideSpinner] = useState(false)
+  const [denyClose, setDenyClose] = useState(true)
 
   // console.log('props.appData: ', props.appData)
   // console.log('props.index: ', props.index)
@@ -31,74 +39,137 @@ function DeleteProduct(props) {
     </>
   )
 
+  // Encapsulate state and state update functions
+  const deleteProdState = {
+    showCCModal,
+    setShowCCModal,
+    showWaitingModal,
+    setShowWaitingModal,
+    waitingModalBody,
+    setWaitingModalBody,
+    hideSpinner,
+    setHideSpinner,
+    denyClose,
+    setDenyClose
+  }
+
   return (
     <>
-    <Button variant='danger' onClick={(e) => handleInitialClick(props, setShowCCModal)}>
+    <Button variant='danger' onClick={(e) => handleInitialClick(props, deleteProdState)}>
       Delete
     </Button>
     {
       showCCModal
         ? (<ModalContinueCancel
-            onCancel={(e) => handleCancel(props, setShowCCModal)}
-            onContinue={(e) => handleContinue(props, setShowCCModal)}
+            onCancel={(e) => handleCancel(props, deleteProdState)}
+            onContinue={(e) => handleContinue(props, deleteProdState)}
             heading="Really Delete Product?"
             body={modalBody}
           />)
+        : null
+    }
+    {
+      showWaitingModal
+        ? (<ModalWaiting
+          heading={`Deleting ${props.productData.name}...`}
+          body={waitingModalBody}
+          denyClose={denyClose}
+          hideSpinner={hideSpinner}
+          closeFunc={waitingModalClose}
+        />)
         : null
     }
     </>
   )
 }
 
-function handleCancel(props, setShowCCModal) {
-  setShowCCModal(false)
-}
-
-function handleContinue(props, setShowCCModal) {
-  console.log('continue button clicked.')
-}
-
-function handleInitialClick(props, setShowCCModal) {
-  setShowCCModal(true)
+// This function is called when the Delete button is clicked. It activates the
+// Cancel/Confirm modal.
+function handleInitialClick(props, deleteProdState) {
+  deleteProdState.setShowCCModal(true)
   return
 }
 
+// This function is called when the Cancel button in the Cancel/Confirm modal
+// is clicked.
+function handleCancel(props, deleteProdState) {
+  deleteProdState.setShowCCModal(false)
+}
+
+// This function is called when the Continue button in the Cancel/Confirm modal
+// is clicked.
+function handleContinue(props, deleteProdState) {
+  console.log('continue button clicked.')
+
+  deleteProdState.setShowCCModal(false)
+  deleteProdState.setShowWaitingModal(true)
+
+  handleDeleteProduct(props, deleteProdState)
+}
+
 // Button click handler. Deletes the product associated with this button.
-async function handleDeleteProduct(props, setShowCCModal) {
+async function handleDeleteProduct(props, deleteProdState) {
   console.log('handleDeleteProduct() actived.')
   // console.log('props: ', props)
 
-  const {appData, index} = props
-
-  let oldMutableData = {}
-  let products = []
   try {
-    oldMutableData = JSON.parse(appData.mutableData)
-    products = oldMutableData.jsonLd.storeData.products
-  } catch (err) {
-    console.log('err: ', err)
-    throw new Error('Could not delete product. Could not retrieve product array from mutable data.')
+    // Update the modal body
+    const modalBody = [`Deleting ${props.productData.name}...`]
+    deleteProdState.setWaitingModalBody(modalBody)
+
+    const {appData, index} = props
+
+    let oldMutableData = {}
+    let products = []
+    try {
+      oldMutableData = JSON.parse(appData.mutableData)
+      products = oldMutableData.jsonLd.storeData.products
+    } catch (err) {
+      console.log('err: ', err)
+      throw new Error('Could not delete product. Could not retrieve product array from mutable data.')
+    }
+
+    // Remove the product from the array
+    products.splice(index, 1)
+
+    // Stringify the mutable data.
+    const mutableDataStr = JSON.stringify(oldMutableData)
+    console.log('Updated mutable data: ', mutableDataStr)
+
+    // Update the modal body
+    modalBody.push(`Writing update to IPFS and BCH blockchain...`)
+    deleteProdState.setWaitingModalBody(modalBody)
+
+    // Generate a transaction to update the mutable data.
+    await updateMutableData({mutableData: mutableDataStr, wallet: appData.wallet})
+
+    // Get updated mutable data
+    console.log('Updating token data cache on server.')
+    await appData.getMutableData(props.appData.wallet, true)
+
+    // Have the SSP API server also update the mutable data for this token.
+    const sspApi = new SspApi()
+    await sspApi.updateStore(oldMutableData.tokenId)
+
+    // Update the modal body
+    modalBody.push(`Done!`)
+    deleteProdState.setWaitingModalBody(modalBody)
+    deleteProdState.setHideSpinner(true)
+
+    // Allow the user to close the waiting modal.
+    deleteProdState.setDenyClose(false)
+  } catch(err) {
+    console.error('Error in handleDeleteProduct(): ', err)
+
+    const modalBody = [`Error: ${err.message}`]
+    deleteProdState.setWaitingModalBody(modalBody)
+    deleteProdState.setHideSpinner(true)
   }
-
-  // Remove the product from the array
-  products.splice(index, 1)
-
-  // Stringify the mutable data.
-  const mutableDataStr = JSON.stringify(oldMutableData)
-  console.log('Updated mutable data: ', mutableDataStr)
-
-  // Generate a transaction to update the mutable data.
-  await updateMutableData({mutableData: mutableDataStr, wallet: appData.wallet})
-
-  // Get updated mutable data
-  console.log('Updating token data cache on server.')
-  await appData.getMutableData(props.appData.wallet, true)
-
-  // Have the SSP API server also update the mutable data for this token.
-  const sspApi = new SspApi()
-  await sspApi.updateStore(oldMutableData.tokenId)
 }
 
-
+// This function is called when the Waiting Modal is closed.
+function waitingModalClose() {
+  console.log('waiting modal closed.')
+}
 
 export default DeleteProduct
